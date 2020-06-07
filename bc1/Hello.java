fa80fef832c01e975a3162cbc205f6007eb63c71
Colin Aylward <50175988+topdeckmagic@users.noreply.github.com>
2020-06-07 16:12:19 +0000

    Homework 1 - Up and Running

---
+++ bc1/Hello.java
@@ -0,0 +1,13 @@
+/**
+ * This is a sample program that will print "Hello, World!" to screen.
+ *
+ * @author  Your Name
+ * @version 1.0
+ */
+public class Hello 
+{
+   public static void main(String[] args) 
+   {
+      System.out.println("Hello, World!");
+   }
+}
